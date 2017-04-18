028f61c7942624e7d8a42302c6bff716487a1885
Jose Ospina <pepo.ospina@gmail.com>
2017-04-18 16:02:34 +0200

    Auth0 Security wip

---
+++ ctwitter-backend/src/main/java/org/collectiveone/ctwitterapi/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package org.collectiveone.ctwitterapi.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
+
+@EnableWebSecurity(debug = true)
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${auth0.issuer}")
+    private String issuer;
+
+    @Value("${auth0.audience}")
+    private String audience;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        JwtWebSecurityConfigurer.forRS256(audience, issuer)
+                .configure(http)
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/secure/**").fullyAuthenticated();
+        //                .antMatchers(HttpMethod.GET, "/secure/**").hasAuthority("read:greeting");
+    }
+}
